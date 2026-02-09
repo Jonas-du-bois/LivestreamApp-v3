1281cf157c8a911acdfeae4eaa4a4ea974c46e12
Jonas Du Bois <jonas.du.bois@outlook.com>
2026-02-10 00:38:56 +0100

    Add initial iOS and Android project files and configurations for Capacitor app

---
+++ LiveStreamApp/android/app/src/main/java/com/fsg/coupedesbains/MainActivity.java
@@ -0,0 +1,14 @@
+package com.fsg.coupedesbains;
+
+import android.os.Bundle;
+import androidx.core.view.WindowCompat;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Edge-to-edge : la WebView s'étend derrière la status bar et la nav bar
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+    }
+}
